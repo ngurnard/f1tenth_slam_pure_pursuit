7c20074a03e80257bdcb9dd89a186ff422ceff95
Mana5a-Sathyan <msathyan@seas.upenn.edu>
2023-02-27 13:31:17 -0500

    Publish Subscribe Nodes

--- pure_pursuit/src/pure_pursuit_node.cpp
+++ pure_pursuit/src/pure_pursuit_node.cpp
@@ -22,15 +22,35 @@ class PurePursuit : public rclcpp::Node
     // This is just a template, you are free to implement your own node!
 
 private:
+    std::string lidarscan_topic = "/scan";
+    std::string drive_topic = "/drive";
+    std::string vis_topic = "";
+
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_sub_;
+    rclcpp::Publisher<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr drive_pub_;
+    rclcpp::Publisher<visualization_msgs::Marker>::SharedPtr vis_pub_;
 
 public:
     PurePursuit() : Node("pure_pursuit_node")
     {
         // TODO: create ROS subscribers and publishers
+        scan_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+            lidarscan_topic, 10, std::bind(&PurePursuit::pose_callback, this, std::placeholders::_1));\
+        
+        drive_pub_ = this->create_publisher<ackermann_msgs::msg::AckermannDriveStamped>(
+            drive_topic, 10);
+
+        vis_pub_ = this->create_publisher<visualization_msgs::Marker>(
+            vis_topic, 10);
+        
     }
 
     void pose_callback(const geometry_msgs::msg::PoseStamped::ConstPtr &pose_msg)
     {
+        angle_min = scan_msg->angle_min;
+        angle_increment = scan_msg->angle_increment;
+
+        const float *range_data = scan_msg->ranges.data();
         // TODO: find the current waypoint to track using methods mentioned in lecture
 
         // TODO: transform goal point to vehicle frame of reference

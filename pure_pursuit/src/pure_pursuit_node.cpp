04ddd9e36f85d3da84994e615c45bd16f6f73e5e
Rithwik Udayagiri <rithwik.math@gmail.com>
2023-02-27 19:43:41 -0500

    Finish pure pursuit

--- pure_pursuit/src/pure_pursuit_node.cpp
+++ pure_pursuit/src/pure_pursuit_node.cpp
@@ -4,10 +4,13 @@
 #include "ackermann_msgs/msg/ackermann_drive_stamped.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "visualization_msgs/msg/marker.hpp"
-#include <tf2_ros/transform_broadcaster.h>
+#include "visualization_msgs/msg/marker_array.hpp"
+#include <tf2_ros/transform_listener.h>
+#include "tf2_ros/buffer.h"
 #include <tf2/LinearMath/Matrix3x3.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <math.h>
+#include "interfaces_hot_wheels/msg/waypoint.hpp"
 /// CHECK: include needed ROS msg type headers and libraries
 
 using namespace std;
@@ -18,10 +21,9 @@ class PurePursuit : public rclcpp::Node
     // This is just a template, you are free to implement your own node!
 
 private:
-    double x, y, v; // position [x,y] and velocity at point, v
+    double x, y, v, L; // position [x,y] and velocity at point, v
     double theta;   // curvature
 
-    
     std::string cur_wpt_topic_ = "/waypoint";
     std::string drive_topic_ = "/drive";
     std::string vis_topic_ = "/vis";
@@ -31,50 +33,63 @@ private:
     rclcpp::Publisher<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr drive_pub_;
     rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr vis_pub_;
 
+
+
 public:
-    PurePursuit() : Node("pure_pursuit_node")
+    PurePursuit() :
+        Node("pure_pursuit_node"),
+        waypoint_sub_(this->create_subscription<interfaces_hot_wheels::msg::Waypoint>(
+            cur_wpt_topic_, 1, std::bind(&PurePursuit::waypoint_callback, this, std::placeholders::_1))),
+        drive_pub_(this->create_publisher<ackermann_msgs::msg::AckermannDriveStamped>(
+            drive_topic_, 1)),
+        vis_pub_(this->create_publisher<visualization_msgs::msg::MarkerArray>(
+            vis_topic_, 1)),
+        x(0.0),
+        y(0.0),
+        v(0.0),
+        L(1.0),
+        theta(0.0)
     {
         // TODO: create ROS subscribers and publishers
-        auto param_desc = rcl_interfaces::msg::ParameterDescriptor{};
-        param_desc.description = "Lookahead distance for Pure Pursuit";
-        this->declare_parameter("L", 1.0, param_desc);  
-        this->declare_parameter("Kp", 0.7); 
-   
-        waypoint_sub_ = this->create_subscription<geometry_msgs::msg::Vector3>(
-            cur_wpt_topic_, 1, std::bind(&PurePursuit::waypoint_callback, this, std::placeholders::_1));
-        
-        drive_pub_ = this->create_publisher<ackermann_msgs::msg::AckermannDriveStamped>(
-            drive_topic_, 1);
-
-        vis_pub_ = this->create_publisher<visualization_msgs::msg::Marker>(
-            vis_topic_, 1);
-
-     
-    }
-
  
-
+        this->declare_parameter("Kp", 0.36);
+    }
     
-    
-    void waypoint_callback(const geometry_msgs::msg::Vector3::ConstPtr &waypoint)
+    void waypoint_callback(const interfaces_hot_wheels::msg::Waypoint::ConstPtr &waypoint)
     {
         // TODO: find the current waypoint to track using methods mentioned in lecture
         x = waypoint->x;
         y = waypoint->y;
         v = waypoint->z;
-        
-        // TODO: transform goal point to vehicle frame of reference
-
-
+        L = waypoint->L;
 
         // TODO: calculate curvature/steering angle
-        theta = 2 * abs(y)/pow(this->get_parameter("L").get_parameter_value().get<float>(), 2);
+        theta = 2 * abs(y)/pow(L, 2);
         
         // TODO: publish drive message, don't forget to limit the steering angle.
+        // kp is proportional to arlength since s=r*theta --> theta is proportional to Kp/r
         ackermann_msgs::msg::AckermannDriveStamped drive_msg;
         drive_msg.drive.steering_angle = this->get_parameter("Kp").get_parameter_value().get<float>() * theta;
         drive_msg.drive.speed = v;
         drive_pub_->publish(drive_msg);
+        
+        // visualization
+        visualization_msgs::msg::MarkerArray marker_array;
+        visualization_msgs::msg::Marker marker;
+
+        marker.type = visualization_msgs::msg::Marker::SPHERE;
+        marker.pose.position.x = x;
+        marker.pose.position.y = y;
+        marker.id = 0;
+        marker.scale.x = 0.1;
+        marker.scale.y = 0.1;
+        marker.scale.z = 0.1;
+        marker.color.a = 1.0;
+        marker.color.r = 0.0;
+        marker.color.g = 1.0;
+        marker.color.b = 0.0;
+        marker_array.markers.push_back(marker);
+        vis_pub_->publish(marker_array);
     }
 
     ~PurePursuit() {}
--- pure_pursuit/src/waypoint_pub.cpp
+++ pure_pursuit/src/waypoint_pub.cpp
@@ -10,12 +10,15 @@ using namespace std;
 #include "nav_msgs/msg/odometry.hpp"
 #include "ackermann_msgs/msg/ackermann_drive_stamped.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/quaternion.hpp"
 #include "visualization_msgs/msg/marker.hpp" // need this
-#include <tf2_ros/transform_broadcaster.h>
+#include "visualization_msgs/msg/marker_array.hpp"
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.h>
 #include <tf2/LinearMath/Matrix3x3.h>
 #include <tf2/LinearMath/Quaternion.h>
 
-#include "interfaces_hot_wheels/msg.Waypoint.hpp"
+#include "interfaces_hot_wheels/msg/waypoint.hpp"
 
 class Waypoint : public rclcpp::Node
 {
@@ -32,69 +35,149 @@ private:
 
     std::vector<interfaces_hot_wheels::msg::Waypoint> waypoints;
 
+    rclcpp::TimerBase::SharedPtr timer_{nullptr};
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_{nullptr};
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
+    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+    std::string source_frame_;
+    std::string target_frame_;
+    
+
 public:
     // default contructor
     Waypoint() : Node("waypoint_node") {
 
-        // auto param_desc = rcl_interfaces::msg::ParameterDescriptor{};
-        // param_desc.description = "Lookahead distance for Pure Pursuit";
-        // this->declare_parameter("L", 1.0, param_desc);  
+        auto param_desc = rcl_interfaces::msg::ParameterDescriptor{};
+        param_desc.description = "Lookahead distance for Pure Pursuit";
+        this->declare_parameter("L", 1.0, param_desc);  
+        source_frame_ = this->declare_parameter<std::string>("source_frame", "/map");
+        target_frame_ = this->declare_parameter<std::string>("target_frame", "/base_link");
+        
+        // populate the waypoints vector
+        csv_to_waypoints();
 
-        wpt_pub_ = this->create_publisher<ackermann_msgs::msg::AckermannDriveStamped>(
+        wpt_pub_ = this->create_publisher<interfaces_hot_wheels::msg::Waypoint>(
             wpt_topic_, 1);
 
-        vis_pub_ = this->create_publisher<visualization_msgs::msg::Marker>(
+        vis_pub_ = this->create_publisher<visualization_msgs::msg::MarkerArray>(
             vis_topic_, 1);
 
         pose_sub_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(
             pose_topic_, 1, std::bind(&Waypoint::pose_callback, this, std::placeholders::_1));
 
-        // populate the waypoints vector
-        csv_to_waypoints();
+
+        // // populate the waypoints vector
+        // csv_to_waypoints();
+
+        tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+        tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
     }
 
     void csv_to_waypoints()
     {
-        string fname;
-        cout<<"Enter the file name: ";
-        cin>>fname;
-    
-        vector<vector<string>> content;
-        vector<string> row;
-        string line, word;
-    
+        string fname = "waypoints/levine.csv";
+        
+        string line;
         fstream file (fname, ios::in);
+
+        visualization_msgs::msg::MarkerArray marker_array;  
+        visualization_msgs::msg::Marker marker;
+        
         if(file.is_open())
         {
+            int marker_id = 1;
             while(getline(file, line))
             {
-                row.clear();
-    
-                stringstream str(line);
-    
-                while(getline(str, word, ','))
-                    row.push_back(word);
-                content.push_back(row);
+                std::vector<std::string> vec;
+                boost::algorithm::split(vec, line, boost::is_any_of(","));
+                interfaces_hot_wheels::msg::Waypoint p;
+
+                p.x = std::stod(vec[0]);
+                p.y = std::stod(vec[1]);
+                p.v = std::stod(vec[2]);
+
+                waypoints.push_back(p);        
+
+                marker.type = visualization_msgs::msg::Marker::SPHERE;
+                marker.pose.position.x = x;
+                marker.pose.position.y = y;
+                marker.id = marker_id++;
+                marker.scale.x = 0.1;
+                marker.scale.y = 0.1;
+                marker.scale.z = 0.1;
+                marker.color.a = 1.0;
+                marker.color.r = 0.0;
+                marker.color.g = 0.0;
+                marker.color.b = 1.0;
+                marker_array.markers.push_back(marker);
             }
         }
+        vis_pub_->publish(marker_array);
     }
 
-    void pose_callback(const geometry_msgs::msg::PoseStamped::ConstPtr &pose)
+    void pose_callback(const geometry_msgs::msg::PoseStamped::ConstPtr &pose_msg)
     {
-        // TODO: find current location
-        x = pose->x;
-        y = pose->y;
-        v = pose->z;
+        float x = pose_msg->pose.position.x;
+        float y = pose_msg->pose.position.y;
+        geometry_msg::msg::Quaternion quat = pose_msg->pose.orientation;
+
+        // TODO: Find optimum waypoint
+
+        // TODO: transform goal point to vehicle frame of reference
+        geometry_msgs::msg::TransformStamped map_to_baselink;
+        std::string SrcFrameRel = source_frame_.c_str();
+        std::string TarFrameRel = target_frame_.c_str();
+
+        try {
+          map_to_baselink = tf_buffer_->lookupTransform(
+            TarFrameRel, SrcFrameRel, // world waypoints in terms of cars frame. From world to car = ^{car}T_{world}
+            tf2::TimePointZero); // get the latest available transform
+        } catch (const tf2::TransformException & ex) {
+          RCLCPP_INFO(
+            this->get_logger(), "Could not transform %s to %s: %s",
+            TarFrameRel.c_str(), SrcFrameRel.c_str(), ex.what());
+          return;
+        }
+
+        // draw it out to see
+        // Pmap is given as x or y
+        // T is the tranform ^{car}T_{world}
+        // Pcar = Pmap + T
+
+        // for each undefined waypoints
+
+        double min_dist = std::numeric_limits<double>::max();
+        interfaces_hot_wheels::msg::Waypoint next_point;
         
-        // TODO: publish waypoint message
-        ackermann_msgs::msg::AckermannDriveStamped drive_msg;
-        drive_msg.drive.steering_angle = this->get_parameter("Kp").get_parameter_value().get<float>() * theta;
-        drive_msg.drive.speed = this->get_parameter("V").get_parameter_value().get<float>();
-        drive_pub_->publish(drive_msg);
+        for (auto wpt : waypoints) {
+            
+            geometry_msgs::Pose wpt_transformed;
+            wpt_transformed.position.x = wpt.x;
+            wpt_transformed.position.y = wpt.y;
+            wpt_transformed.position.z = 0;
+            wpt_transformed.orientation.x = 0;
+            wpt_transformed.orientation.y = 0;
+            wpt_transformed.orientation.z = 0;
+            wpt_transformed.orientation.w = 1;
+            tf2::doTransform(wpt_transformed, wpt_transformed, map_to_baselink);
 
-        // TODO: publish vis message
+            if(wpt_transformed.x < 0.0){
+                continue;
+            }
+
+            double dist = sqrt(pow(wpt_transformed.x, 2) + pow(wpt_transformed.y, 2));
+            if(dist < min_distance && dist > this->get_parameter("L").get_parameter_value().get<float>()){
+                min_dist = dist;
+                next_point.x = wpt_transformed.x;
+                next_point.y = wpt_transformed.y;
+        }
+
+        next_point.v = 2.0;
+        next_point.L = this->get_parameter("L").get_parameter_value().get<float>();
+        wpt_pub_->publish(next_point);
+        
     }
-
+    
     ~Waypoint() {}
 };
 

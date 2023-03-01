c186f037968f4276635bfa1b5a70f60fa01eb75a
Rithwik Udayagiri <rithwik.math@gmail.com>
2023-02-28 23:31:45 -0500

    remap topics for real car

--- pure_pursuit/launch/race_pure_pursuit_launch.py
+++ pure_pursuit/launch/race_pure_pursuit_launch.py
@@ -6,11 +6,17 @@ def generate_launch_description():
         Node(
             package='pure_pursuit',
             executable='pure_pursuit_node',
-            name='pure_pursuit_node'
+            name='pure_pursuit_node',
+            remappings=[
+            ("pf/viz/inferred_pose", "pose")
+            ]
         ),
         Node(
             package='pure_pursuit',
             executable='waypoint_node',
-            name='waypoint_node'
+            name='waypoint_node',
+            remappings=[
+            ("pf/viz/inferred_pose", "pose")
+            ]
         )
     ])

dbad9298a0b4a51e8ca6f686c98b18755457b37e
Rithwik Udayagiri <rithwik.math@gmail.com>
2023-02-28 23:10:53 -0500

    Edit waypoint logger for car

--- pure_pursuit/scripts/waypoint_logger.py
+++ pure_pursuit/scripts/waypoint_logger.py
@@ -7,15 +7,16 @@ from os.path import expanduser
 from time import gmtime, strftime
 from numpy import linalg as LA
 from nav_msgs.msg import Odometry
+from geometry_msgs.msg import PoseStamped
 import math
 from tf_transformations import euler_from_quaternion  # sudo apt install ros-foxy-tf-transformations 
 import time
 
 
-relative_path = "/sim_ws/src/pure_pursuit/pure_pursuit/waypoints/"
-fname = "waypoints1.csv"
+relative_path = "/home/nvidia/f1tenth_ws/src/pure_pursuit/pure_pursuit/waypoints/"
+fname = "waypoints_drive"
 # file = open(strftime(relative_path+'waypoint-%Y-%m-%d-%H-%M-%S', gmtime())+'.csv', 'w')
-file = open(relative_path+'waypoint_drive.csv', 'w')
+file = open(relative_path+fname+'.csv', 'w')
 
 
 previous_time = 0.0
@@ -25,8 +26,8 @@ class WaypointLogger(Node):
     def __init__(self):
         super().__init__('minimal_subscriber')
         self.subscription = self.create_subscription(
-            Odometry,
-            'ego_racecar/odom',
+            PoseStamped,
+            'pf/viz/inferred_pose',
             self.save_waypoint,
             10)
         self.subscription  # prevent unused variable warning
@@ -35,18 +36,19 @@ class WaypointLogger(Node):
     def save_waypoint(self,  data):
         global previous_time
         if(data.header.stamp.sec - previous_time >= 2):
-            quaternion = np.array([data.pose.pose.orientation.x, 
-                                data.pose.pose.orientation.y, 
-                                data.pose.pose.orientation.z, 
-                                data.pose.pose.orientation.w])
+            quaternion = np.array([data.pose.orientation.x, 
+                                data.pose.orientation.y, 
+                                data.pose.orientation.z, 
+                                data.pose.orientation.w])
 
             euler = euler_from_quaternion(quaternion)
-            speed = LA.norm(np.array([data.twist.twist.linear.x, 
-                                    data.twist.twist.linear.y, 
-                                    data.twist.twist.linear.z]),2)
+            speed = 3.0
+            # speed = LA.norm(np.array([data.twist.twist.linear.x, 
+            #                         data.twist.twist.linear.y, 
+            #                         data.twist.twist.linear.z]),2)
 
-            file.write('%f, %f, %f, %f\n' % (data.pose.pose.position.x,
-                                            data.pose.pose.position.y,
+            file.write('%f, %f, %f, %f\n' % (data.pose.position.x,
+                                            data.pose.position.y,
                                             euler[2],
                                             speed))
             print("Every 2sec", previous_time)

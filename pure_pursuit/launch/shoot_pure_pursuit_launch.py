de9f2a6f9df5932a90f2215ef4ab93b135d124a1
Rithwik Udayagiri <rithwik.math@gmail.com>
2023-03-27 22:57:00 -0400

    Add optimized raceline with margin

--- pure_pursuit/launch/shoot_pure_pursuit_launch.py
+++ pure_pursuit/launch/shoot_pure_pursuit_launch.py
@@ -33,7 +33,7 @@ def generate_launch_description():
                 'global_frame'   : "map",
                 'local_frame'    : "laser",
                 'waypoints_path' : share_directory,
-                'waypoints_file' : "waypoints_optimized_2.csv",
+                'waypoints_file' : "waypoints_optimized_7.csv",
                 'v_csv'          :  0,
                 'v'              :  3.0
                 }

0de9752f993bc68c3ebcae115da8ea4ba8f01e9f
Rithwik Udayagiri <rithwik.math@gmail.com>
2023-04-22 13:52:42 -0400

    Add lane switcher launch file for real car

---
+++ pure_pursuit/launch/laneswitcher_race_pure_pursuit_launch.py
@@ -0,0 +1,54 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch_ros.substitutions import FindPackageShare
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument
+
+def generate_launch_description():
+    share_directory = os.path.join(
+        get_package_share_directory('pure_pursuit'),
+        'waypoints', "")
+    return LaunchDescription([
+        Node(
+            package='pure_pursuit',
+            executable='pure_pursuit_node',
+            name='pure_pursuit_node',
+            parameters=[
+            {
+            'Kp' : 0.4
+            }
+            ]
+        ),
+        Node(
+            package='pure_pursuit',
+            executable='waypoint_laneswitcher_node',
+            name='waypoint_laneswitcher_node',
+            parameters=[
+                {
+                'global_frame'      : "map",
+                'local_frame'       : "laser",
+                'waypoints_path'    : share_directory,
+                'v_csv'             : 0,
+                'v'                 : 3.0,
+                'L_csv'             : 1,
+                'L'                 : 1.2,
+                'straight_opp_dist' : 2.0,
+                'side_opp_dist'     : 0.5,
+                }
+            ]
+        ),
+        IncludeLaunchDescription
+        (
+                PythonLaunchDescriptionSource
+                ([
+                    FindPackageShare("particle_filter"),
+                    '/launch',
+                    '/localize_launch.py'
+                ])
+        ) 
+    ])
